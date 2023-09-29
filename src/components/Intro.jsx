a9067033bb77a43fc6d7baabde8052c37fe3d3b3
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-09-29 23:50:45 +0900

    인트로 작업

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,46 @@
-
 const Intro = () => {
   return (
-      <div>Intro</div>
+      <section id="intro">
+        <div className="intro__inner">
+          <h2 className="intro__title">
+            port developer
+          </h2>
+          <div className="intro__lines" aria-hidden="true">
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+          </div>
+          <div className="intro__text">
+            <div className="text">
+              <div>talent is</div>
+              <div>found at the end of the</div>
+              <div>effort</div>
+            </div>
+            <div className="img">
+              <Image
+                  src="/assets/img/about.jpg"
+                  alt="어바웃"
+                  width={300}
+                  height={300}
+              />
+            </div>
+          </div>
+          <div className="intro__lines bottom" aria-hidden="true">
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+          </div>
+        </div>
+      </section>
   )
 }
 
-export default Intro
+export default Intro;
